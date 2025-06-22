f05cdca92ada7d553638420d63a5b2e542e7e609
theraw007 <sanjayrawat479@gmail.com>
2025-06-22 20:26:58 +0530

    Add files via upload

---
+++ Personalized_Greeting.py
@@ -0,0 +1,3 @@
+a=input("Enter your First Name: ")
+b=input("Enter your Last Name: ")
+print("Hello,", a, b,"! Welcom to the Python Programme")
---
+++ Simple_Calculator.py
@@ -0,0 +1,8 @@
+a=input("Enter the First Number:")
+b=input("Enter the Second Number:")
+a=int(a)
+b=int(b)
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
