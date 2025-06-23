bd32b0e6699a2974722c3ce66d212c0641029d0d
theraw007 <sanjayrawat479@gmail.com>
2025-06-23 21:37:50 +0530

    Add files via upload

---
+++ Number is Even or Odd.py
@@ -0,0 +1,7 @@
+Num=int(input("Enter the Number:"))
+if Num%2==0:
+    print(Num, "is a Even Number")
+else:
+    print(Num,"is a odd number")
+
+    print("Thank You")
---
+++ Sum of a Integer from 1 to 50 using a loop.py
@@ -0,0 +1,6 @@
+Total_sum=0
+for i in range(1,51):
+    Total_sum=Total_sum+i
+    print("The Sum of Number 1 to 50 is:", Total_sum)
+
+
